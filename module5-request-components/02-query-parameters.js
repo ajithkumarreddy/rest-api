ebba01d3b8fe2c726e8784ba7158caeea5ef3051
Ajith Kumar <ajithkumarreddy123@gmail.com>
2026-07-15 23:02:21 +0530

    feat: added query params and body

---
+++ module5-request-components/02-query-parameters.js
@@ -0,0 +1,24 @@
+/**
+ * Query parameters
+ * These come after the question mark (?)
+ * GET /products?category=mobile
+ */
+
+app.get("/products", (req, res) => {
+  console.log(req.query);
+
+  const { category } = req.query;
+  res.send(category);
+});
+
+/**
+ * Multiple Query parameters
+ * GET /products?category=mobile&page=20&sort=price
+ */
+
+app.get("/products", (req, res) => {
+  console.log(req.query);
+
+  const { category, page, sort } = req.query;
+  res.send({ category, page, sort });
+});
---
+++ module5-request-components/03-request-body.js
@@ -0,0 +1,25 @@
+/**
+ * Request body
+ * The body carries actual data.
+ * Usually with POST, PUT, PATCH
+ */
+
+const newUser = {
+  name: "Rama",
+  age: 26,
+};
+
+const express = require("express");
+
+// server
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  res.send("new user created");
+});
+
+app.listen(3000);
